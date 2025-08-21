65c1dc6335f07482b4aac401cf717ff34a231094
GustavoArAz <gustavoaa2011@gmail.com>
2025-08-21 11:18:10 -0300

    Progresso final dia 21/08

--- dundie/app.py
+++ dundie/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from .routes import main_router
+
 
 app = FastAPI(
     title="dundie",
     version="0.1.0",
     description="dundie is a test for rewards API",
 )
+
+app.include_router(main_router)
